d115db76f2990416e1fa2f75dffc0c74ec3ed5a7
H P <nurchumairohahmad@gmail.com>
2024-09-24 11:53:32 +0700

    lanjut

--- src/main/kotlin/listOf.kt
+++ src/main/kotlin/listOf.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
     val people = listOf<Person>(Person("Adam", 20), bob, bob)
     val people2 = listOf<Person>(Person("Adam", 20), Person("Bob", 31), bob)
     println(people == people2)
-    bob.age = 12
+    bob.age = 32
     println(people == people2)
 }
 
